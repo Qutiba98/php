d572d1073209e43ac608bb94636f46d94c066de8
qutiba arabiyat <qutipa.arabiyat@gmail.com>
2024-07-09 10:10:29 +0300

    Update php.php

--- 9-7-2024/php.php
+++ 9-7-2024/php.php
@@ -141,6 +141,20 @@
     #Q_9
 
 
+echo "<br><br><br>Q9<br><br>"; 
+
+    echo '<table border="1" cellpadding="3" cellspacing="0">';
+
+    for ($i = 1; $i <=6; $i++) {
+        echo "<tr>";
+        for ($j = 1; $j <= 5; $j++) {
+            $sum = $i * $j;
+            echo "<td style='padding-right: 80px'>" . $i . " * " . $j . " = " . $sum . "</td>";
+        }
+        echo "</tr>";
+    }
+
+    echo '</table>';
 
 
 
